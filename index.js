562926e9d7bed884fd1a1b0139e1ed139458f779
swdrana <tctr2s@gmail.com>
2022-05-23 21:58:17 +0600

    add product api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 //For Middleware
 const app = express();
@@ -11,54 +11,53 @@ app.use(express.json());
             MongoDB Connection Start
 \********************************************/
 //Import
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1z0dn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
-  });
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-  const run = async () => {
-    try {
-      await client.connect();
-      const productsCollection = client.db("eToolsDB").collection("products");
-  
-      // load all item from database
-      app.get("/products", async (req, res) => {
-        const query = {};
-        const cursor = productsCollection.find(query);
-        const allProduct = await cursor.toArray();
-        res.send(allProduct);
-      });
-  
-      
-    } finally {
-      // await client.close();
-    }
-  };
-  run().catch(console.dir);
+const run = async () => {
+  try {
+    await client.connect();
+    const productsCollection = client.db("eToolsDB").collection("products");
 
+    // load all item from database
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const allProduct = await cursor.toArray();
+      res.send(allProduct);
+    });
 
-
-
-
-
+    // add single item to database
+    app.post("/add", async (req, res) => {
+      const newItem = req.body;
+      console.log(newItem);
+      res.send({ result: "data received!" });
+      const result = await productsCollection.insertOne(newItem);
+      console.log("User Inserted. ID: ", result.insertedId);
+    });
+  } finally {
+    // await client.close();
+  }
+};
+run().catch(console.dir);
 
 /********************************************\
             MongoDB Connection End
 \********************************************/
 
-
-
 // Create Root API
-app.get('/', (req, res)=>{
-    res.send("Running eTools Server")
+app.get("/", (req, res) => {
+  res.send("Running eTools Server");
 });
 
 // For Port & Listening
 const port = process.env.PORT || 8080;
-app.listen(port, (req, res)=>{
-    console.log("Listening to port: ", port);
+app.listen(port, (req, res) => {
+  console.log("Listening to port: ", port);
 });
