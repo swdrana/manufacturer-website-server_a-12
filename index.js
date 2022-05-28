be75f3c1e563948c9784c9aa98f73f1f303278b0
swdrana <tctr2s@gmail.com>
2022-05-28 20:55:32 +0600

    JWT implemented

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// for JWT 
+const accessToken = async (req, res, next) =>{
+
+}
+
 /********************************************\
             MongoDB Connection Start
 \********************************************/
